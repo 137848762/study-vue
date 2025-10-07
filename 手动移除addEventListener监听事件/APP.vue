c0b1b6aeaed814cc755de7c2231e4044b57cb733
137848762 <90426707+137848762@users.noreply.github.com>
2025-10-08 06:44:31 +0800

    Create APP.vue

---
+++ 手动移除addEventListener监听事件/APP.vue
@@ -0,0 +1,32 @@
+<template>
+	<div ref="myElement">点击我</div>
+</template>
+<script>
+	export default{
+		mounted() {
+			// 在组件挂载时添加监听事件
+			this.$refs.myElement.addEventListener('click',this.handleClick);
+			console.log("1");
+		},
+		beforeDestroy() {
+			// 在组件销毁前移除监听事件
+			this.$refs.myElement.removeEventListener('click',this.handleClick);
+			console.log("2");
+		},
+		methods:{
+			handleClick(event){
+				console.log("事件被点击了");
+			}
+		}
+	}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
